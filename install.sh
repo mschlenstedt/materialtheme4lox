7f150eb87c480705018c4a052dce03b36cccc55c
mschlenstedt <Michael@schlenn.net>
2019-12-25 12:42:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ if [ ! -d "/var/lib/squeezeboxserver/cache/InstalledPlugins/Plugins/MaterialSkin
 		echo "Good bye."
 		exit 0
 	fi
-	if [ ! -e "$folder/HTML/material/html/lib/vuetify.min.css" ]; then
+	if [ ! -e "$folder/HTML/material/html/lib/lib.min.css" ]; then
 		echo "Cannot find installation folder of Material Theme in $folder. Giving up."
 		echo "Good bye."
 		exit 0
@@ -29,15 +29,15 @@ fi
 echo "Found Material Theme in $folder"
 
 # Changing files
-echo "Backing up $folder/vuetify.min.css to vuetify.min.css.backup"
-cp $folder/vuetify.min.css $folder/vuetify.min.css.backup
+echo "Backing up $folder/lib.min.css to lib.min.css.backup"
+cp $folder/lib.min.css $folder/vlib.min.css.backup
 
-echo "Backing up $folder/vuetify.min.js to vuetify.min.js.backup"
-cp $folder/vuetify.min.js $folder/vuetify.min.js.backup
+echo "Backing up $folder/lib.min.js to lib.min.js.backup"
+cp $folder/lib.min.js $folder/vlib.min.js.backup
 
 echo "Changing colors..."
-sed -i 's/#1976D2/#82B827/g' $folder/vuetify.min.css
-sed -i 's/#1976D2/#82B827/g' $folder/vuetify.min.js
-sed -i 's/.theme--dark.v-toolbar{background-color:#212121/.theme--dark.v-toolbar{background-color:#82B827/' $folder/vuetify.min.css
+sed -i 's/#1976D2/#82B827/g' $folder/lib.min.css
+sed -i 's/#1976D2/#82B827/g' $folder/lib.min.js
+sed -i 's/.theme--dark.v-toolbar{background-color:#212121/.theme--dark.v-toolbar{background-color:#82B827/' $folder/lib.min.css
 
 echo "Done. Good bye."
